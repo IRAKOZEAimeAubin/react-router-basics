e089ff7c3cc060896aa3a107299d198b3a2bdead
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-06-21 17:57:25 +0200

    feat: refactor code in react-router v6 format

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom"
+import RootLayout from "./layouts/RootLayout"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import HelpLayout from "./layouts/HelpLayout";
+import Faq from "./pages/help/Faq";
+import Contact from "./pages/help/Contact";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={ <RootLayout /> }>
+      <Route index element={ <Home /> } />
+      <Route path="about" element={ <About /> } />
+      <Route path="help" element={ <HelpLayout /> }>
+        <Route path="faq" element={ <Faq /> } />
+        <Route path="contact" element={ <Contact /> } />
+      </Route>
+      <Route path="*" element={ <NotFound /> } />
+    </Route>
+  )
+)
+
 const App = () => {
   return (
-    <div className="">Hello, 44</div>
+    <RouterProvider router={ router } />
   )
 }
 
