7733e7d645d8c4a720c09144f95a7ed865ba94aa
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-06-26 12:56:23 +0200

    feat: add navigate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
+import { Navigate } from 'react-router-dom';
 
 const About = () => {
+    const [ user, setUser ] = useState( "dash" )
+    
+    if ( !user ) {
+        return <Navigate to="/" replace={ true } />
+    }
+
     return (
         <div>
             <h2>About Us</h2>
             <p className="my-5 mx-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
             <p className="my-5 mx-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
-            <p className="my-5 mx-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p> 
+            <p className="my-5 mx-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
+            <button onClick={ () => setUser( null ) } className="p-2 rounded uppercase bg-primary cursor-pointer tracking-wider font-semibold">Logout</button>
         </div>
     )
 }
